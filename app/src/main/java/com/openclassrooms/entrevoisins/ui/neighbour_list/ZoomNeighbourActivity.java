8694d307d74e66cea7bb217f1822c3759643348b
Sandrine <61731368+elonaenjy@users.noreply.github.com>
2021-01-14 14:25:47 +0100

    le bouton favorit apparait dans la barre réduite

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ZoomNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ZoomNeighbourActivity.java
@@ -1,12 +1,15 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,9 +36,10 @@ public class ZoomNeighbourActivity extends AppCompatActivity {
     private TextView mZoomPhone;
     private TextView mZoomReseau;
     private TextView mZoomAboutMe;
+    private Menu menu;
 
 
-        @Override
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             mFavApiService = DI.getNeighbourApiService();
             super.onCreate( savedInstanceState );
@@ -69,7 +73,6 @@ public class ZoomNeighbourActivity extends AppCompatActivity {
             nom = neighbour.getName();
             mZoomReseau.append("www.Facebook.com/"+nom);
             isFavorite = neighbour.isFavorit();
-            Log.e("TAG3", "alimentation du booleen : "+isFavorite);
 
             fab = findViewById( R.id.fab );
             if (isFavorite) {
@@ -84,7 +87,62 @@ public class ZoomNeighbourActivity extends AppCompatActivity {
 
             fabOnclickListener();
 
+            AppBarLayout mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+            mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                boolean isShow = false;
+                int scrollRange = -1;
+
+                @Override
+                public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                    if (scrollRange == -1) {
+                        scrollRange = appBarLayout.getTotalScrollRange();
+                    }
+                    if (scrollRange + verticalOffset == 0) {
+                        isShow = true;
+                        showOption(R.id.favorit_zoom);
+                    } else if (isShow) {
+                        isShow = false;
+                        hideOption(R.id.favorit_zoom);
+                    }
+                }
+            });
         }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.zoom_menu, menu);
+        hideOption(R.id.favorit_zoom);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.favorit_zoom) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void hideOption(int id) {
+        MenuItem item = menu.findItem(id);
+        item.setVisible(false);
+    }
+
+    private void showOption(int id) {
+        MenuItem item = menu.findItem(id);
+        item.setVisible(true);
+    }
 
     private void fabOnclickListener() {
         fab.setOnClickListener(new View.OnClickListener() {
